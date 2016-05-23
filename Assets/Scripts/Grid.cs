b831c2bb38eae2d967842cb2dbfc43783829f25b
gozu66 <gozu66@gmail.com>
2016-05-24 00:56:35 +0100

    A* Node and Grid classes

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour
+{
+    public LayerMask unWalkableMask;
+    public Vector2 worldSize;
+
+    Node[,] grid;
+
+    public float nodeRadius;
+    float nodeDiameter;
+
+    int gridSizeX, gridSizeY;
+
+    void Start()
+    {
+        nodeDiameter = nodeRadius * 2;
+
+        gridSizeX = Mathf.RoundToInt(worldSize.x / nodeDiameter);
+        gridSizeY = Mathf.RoundToInt(worldSize.y / nodeDiameter);
+
+        CreateGrid();
+    }
+    
+    void CreateGrid()
+    {
+        grid = new Node[gridSizeX, gridSizeY];
+
+        Vector3 worldBottomLeft = transform.position - Vector3.right * gridSizeX / 2 - transform.position - Vector3.forward * gridSizeY / 2;
+
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            for(int y = 0; y < gridSizeY; y++)
+            {
+                Vector3 worldPosition = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
+                bool walkable = !(Physics.CheckSphere(worldPosition, nodeRadius, unWalkableMask));
+                grid[x, y] = new Node(walkable, worldPosition);
+            }
+        }
+    }
+
+    public Node NodeFromWorldPoint(Vector3 worldPosition)
+    {
+        //float percentX = (worldPosition.x + worldSize.x / 2) / worldSize.x;     //Unoptimized
+        //float percentY = (worldPosition.z + worldSize.y / 2) / worldSize.y;     //Unoptimized
+        
+        float percentX = worldPosition.x / worldSize.x + 0.5f;      //Optimized
+        float percentY = worldPosition.z / worldSize.y + 0.5f;      //Optimized
+        
+        percentX = Mathf.Clamp01(percentX);
+        percentY = Mathf.Clamp01(percentY);
+        int x = Mathf.RoundToInt((gridSizeX - 1) * percentX);
+        int y = Mathf.RoundToInt((gridSizeY - 1) * percentY);
+        return grid[x, y];
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(worldSize.x, 1, worldSize.y));
+        
+        if(grid != null)
+        {
+            foreach(Node n in grid)
+            {
+                Gizmos.color = (n.walkable) ? Color.white : Color.red;
+                Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - 0.1f));
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # A_Star
 My implementation of the A* path-finding algorithm.
+
+With help from [Sebastian Lague's](https://github.com/SebLague) A* [tutorial series](https://www.youtube.com/playlist?list=PLFt_AvWsXl0cq5Umv3pMC9SPnKjfp9eGW)
